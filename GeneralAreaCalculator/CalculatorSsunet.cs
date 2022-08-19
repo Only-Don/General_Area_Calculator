a3d57c56929fe78c88de88d3af162ecf5432217d
ssunet <640830540@qq.com>
2022-08-19 17:49:55 +0800

    ssunet1

---
+++ GeneralAreaCalculator/CalculatorSsunet.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+//using Record;
+
+//namespace GeneralAreaCalculator
+//{
+//    public partial class Form1 : Form
+//    {
+//        HistoryRecord record = new HistoryRecord();
+//        public Form1()
+//        {
+//            InitializeComponent();
+//        }
+
+//        private void Load_Click(object sender, EventArgs e)
+//        {
+//            record.ShowRecord(this.textBox3);
+//        }
+
+//        private void Save_Click(object sender, EventArgs e)
+//        {
+//            record.SaveRecord();
+//        }
+
+//        private void calculate_Click(object sender, EventArgs e)
+//        {
+//            string output = "";//此处取输出结果
+
+
+//            record.GetRecord(output, this.comboBox1.Text, this.comboBox2.Text);
+//        }
+//    }
+//}
---
+++ GeneralAreaCalculator/record.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Record
+{
+    public class HistoryRecord
+    {
+        List<string> records = new List<string>();
+
+        public HistoryRecord()
+        {
+            FileStream recordFile = new FileStream("records.txt", FileMode.OpenOrCreate);
+            StreamReader streamReader = new StreamReader(recordFile);
+            while (streamReader.Peek() >= 0)
+            {
+                records.Add(streamReader.ReadLine());
+            }
+        }
+
+        //运算结束时调用
+        public void GetRecord(string ouput, string graphicsType,string unitType)
+        {
+            records.Add($"{graphicsType} {unitType} {ouput}");
+        }
+
+        //保存时调用
+        public void SaveRecord()
+        {
+            FileStream recordFile = new FileStream("records.txt",FileMode.Append);
+            StreamWriter streamWriter = new StreamWriter(recordFile);
+
+            foreach (var record in records)
+            {
+                streamWriter.WriteLine(record);
+            }
+            streamWriter.Close();
+        }
+
+        //点击查看历史记录时调用
+        public void ShowRecord(TextBox recordTextBox)
+        {
+            recordTextBox.Text = string.Join("\r\n", records);
+        }
+
+    }
+}
+
