4e686db77b4296bc0c5cc6a94c2d762dd6af2ee4
Only-Don <Only_Don@163.com>
2022-08-19 16:14:57 +0800

    Design a Graphical User Interface

---
+++ GeneralAreaCalculator/Calculator.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace GeneralAreaCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SqaureLable = new System.Windows.Forms.Label();
+            this.RectangleLengthLable = new System.Windows.Forms.Label();
+            this.RectangleWidthLable = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TriangleBaseLable = new System.Windows.Forms.Label();
+            this.TriangleHeightLable = new System.Windows.Forms.Label();
+            this.CircleLable = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Load = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(220, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "通用面积计算器";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(63, 149);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(179, 32);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(59, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "图形";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(275, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "单位";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(279, 149);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(179, 32);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 18F);
+            this.textBox1.Location = new System.Drawing.Point(508, 146);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 35);
+            this.textBox1.TabIndex = 5;
+            // 
+            // SqaureLable
+            // 
+            this.SqaureLable.AutoSize = true;
+            this.SqaureLable.Font = new System.Drawing.Font("宋体", 18F);
+            this.SqaureLable.Location = new System.Drawing.Point(504, 113);
+            this.SqaureLable.Name = "SqaureLable";
+            this.SqaureLable.Size = new System.Drawing.Size(58, 24);
+            this.SqaureLable.TabIndex = 6;
+            this.SqaureLable.Text = "边长";
+            this.SqaureLable.UseWaitCursor = true;
+            this.SqaureLable.Visible = false;
+            // 
+            // RectangleLengthLable
+            // 
+            this.RectangleLengthLable.AutoSize = true;
+            this.RectangleLengthLable.Font = new System.Drawing.Font("宋体", 18F);
+            this.RectangleLengthLable.Location = new System.Drawing.Point(504, 113);
+            this.RectangleLengthLable.Name = "RectangleLengthLable";
+            this.RectangleLengthLable.Size = new System.Drawing.Size(34, 24);
+            this.RectangleLengthLable.TabIndex = 8;
+            this.RectangleLengthLable.Text = "长";
+            this.RectangleLengthLable.UseWaitCursor = true;
+            this.RectangleLengthLable.Visible = false;
+            // 
+            // RectangleWidthLable
+            // 
+            this.RectangleWidthLable.AutoSize = true;
+            this.RectangleWidthLable.Font = new System.Drawing.Font("宋体", 18F);
+            this.RectangleWidthLable.Location = new System.Drawing.Point(504, 199);
+            this.RectangleWidthLable.Name = "RectangleWidthLable";
+            this.RectangleWidthLable.Size = new System.Drawing.Size(34, 24);
+            this.RectangleWidthLable.TabIndex = 10;
+            this.RectangleWidthLable.Text = "宽";
+            this.RectangleWidthLable.UseWaitCursor = true;
+            this.RectangleWidthLable.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 18F);
+            this.textBox2.Location = new System.Drawing.Point(508, 232);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 35);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Visible = false;
+            // 
+            // TriangleBaseLable
+            // 
+            this.TriangleBaseLable.AutoSize = true;
+            this.TriangleBaseLable.Font = new System.Drawing.Font("宋体", 18F);
+            this.TriangleBaseLable.Location = new System.Drawing.Point(504, 113);
+            this.TriangleBaseLable.Name = "TriangleBaseLable";
+            this.TriangleBaseLable.Size = new System.Drawing.Size(34, 24);
+            this.TriangleBaseLable.TabIndex = 12;
+            this.TriangleBaseLable.Text = "底";
+            this.TriangleBaseLable.UseWaitCursor = true;
+            this.TriangleBaseLable.Visible = false;
+            // 
+            // TriangleHeightLable
+            // 
+            this.TriangleHeightLable.AutoSize = true;
+            this.TriangleHeightLable.Font = new System.Drawing.Font("宋体", 18F);
+            this.TriangleHeightLable.Location = new System.Drawing.Point(504, 199);
+            this.TriangleHeightLable.Name = "TriangleHeightLable";
+            this.TriangleHeightLable.Size = new System.Drawing.Size(34, 24);
+            this.TriangleHeightLable.TabIndex = 14;
+            this.TriangleHeightLable.Text = "高";
+            this.TriangleHeightLable.UseWaitCursor = true;
+            this.TriangleHeightLable.Visible = false;
+            // 
+            // CircleLable
+            // 
+            this.CircleLable.AutoSize = true;
+            this.CircleLable.Font = new System.Drawing.Font("宋体", 18F);
+            this.CircleLable.Location = new System.Drawing.Point(504, 113);
+            this.CircleLable.Name = "CircleLable";
+            this.CircleLable.Size = new System.Drawing.Size(58, 24);
+            this.CircleLable.TabIndex = 15;
+            this.CircleLable.Text = "直径";
+            this.CircleLable.UseWaitCursor = true;
+            this.CircleLable.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(63, 232);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(395, 184);
+            this.textBox3.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(59, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 24);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "图形";
+            // 
+            // Load
+            // 
+            this.Load.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Load.Location = new System.Drawing.Point(508, 346);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(121, 70);
+            this.Load.TabIndex = 18;
+            this.Load.Text = "加载";
+            this.Load.UseVisualStyleBackColor = true;
+            // 
+            // Save
+            // 
+            this.Save.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Save.Location = new System.Drawing.Point(653, 346);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(121, 70);
+            this.Save.TabIndex = 19;
+            this.Save.Text = "保存";
+            this.Save.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(504, 304);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 24);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "历史记录";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.CircleLable);
+            this.Controls.Add(this.TriangleHeightLable);
+            this.Controls.Add(this.TriangleBaseLable);
+            this.Controls.Add(this.RectangleWidthLable);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RectangleLengthLable);
+            this.Controls.Add(this.SqaureLable);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label SqaureLable;
+        private System.Windows.Forms.Label RectangleLengthLable;
+        private System.Windows.Forms.Label RectangleWidthLable;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label TriangleBaseLable;
+        private System.Windows.Forms.Label TriangleHeightLable;
+        private System.Windows.Forms.Label CircleLable;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
